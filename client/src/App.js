c435fc9b715c4daf3294280a2fde45c0f0ff2c77
Shubham Sapra <shubham.sapra97@gmail.com>
2021-01-16 23:51:02 +0530

    client react app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import ProductList from "./components/ProductList/ProductList";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ProductList />
+      </header>
+    </div>
+  );
+}
+
+export default App;
